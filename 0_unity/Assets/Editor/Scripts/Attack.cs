5c43d1f002294b876658c3691dc80c46d482b741
Vincent Mahnke <vincent@mahn.ke>
2020-03-29 23:40:18 +0200

    Added editor for single and AoE attacks Added example Aoe and Single target attack instances

---
+++ 0_unity/Assets/Editor/Scripts/Attack.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+namespace DataEditor
+{
+    [CustomEditor(typeof(Data.SingleTargetAttack))]
+    [CanEditMultipleObjects]
+    public class SingleTargetAttack : Editor
+    { 
+        private static readonly string[] propertiesToWatch = { "displayName", "icon", "regularAttackValue", "isPositional" }; 
+        Dictionary<string, SerializedProperty> properties;
+        
+        void OnEnable()
+        {
+            properties = propertiesToWatch
+                .Select(propertyName => new KeyValuePair<string, SerializedProperty>(propertyName, serializedObject.FindProperty(propertyName)))
+                .ToDictionary(item => item.Key, item => item.Value);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            foreach (SerializedProperty property in properties.Values)
+            {
+                EditorGUILayout.PropertyField(property);
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+    
+    [CustomEditor(typeof(Data.AoEAttack))]
+    [CanEditMultipleObjects]
+    public class AoEAttack : Editor
+    { 
+        private static readonly string[] propertiesToWatch = { "displayName", "icon", "attackValueAtCenter", "radius", "falloffByDistance" }; 
+        Dictionary<string, SerializedProperty> properties;
+        
+        void OnEnable()
+        {
+            SceneView.duringSceneGui += this.OnSceneGUI;
+
+            properties = propertiesToWatch
+                .Select(propertyName => new KeyValuePair<string, SerializedProperty>(propertyName, serializedObject.FindProperty(propertyName)))
+                .ToDictionary(item => item.Key, item => item.Value);
+        }
+
+        void OnSceneGUI(SceneView sceneView)
+        {
+            Data.AoEAttack item = (Data.AoEAttack)target;
+
+            Handles.color = new Color(1, 0, 0, 0.1f);
+
+            Vector3 mousePosition = Event.current.mousePosition;
+            Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
+            
+            if (Physics.Raycast(ray, out RaycastHit hit)) {
+                Handles.DrawSolidDisc(hit.point, hit.normal, item.Radius);
+            }
+
+            SceneView.RepaintAll();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            Data.AoEAttack item = (Data.AoEAttack)target;
+
+            serializedObject.Update();
+            foreach (SerializedProperty property in properties.Values)
+            {
+                EditorGUILayout.PropertyField(property);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ 0_unity/Assets/Scripts/Data/Attacks/Attack.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    public class Attack : ScriptableObject
+    {
+        [SerializeField]
+        private string displayName;
+        public string DisplayName => displayName;
+        
+        [SerializeField]
+        private Texture2D icon;
+        public Texture2D Icon => icon;
+    }
+    
+    [CreateAssetMenu(fileName = "SingleTargetAttack", menuName = "Data/Attack/Single Target", order = 100)]
+    public class SingleTargetAttack : Attack
+    {
+        [SerializeField]
+        private float regularAttackValue;
+        public float RegularAttackValue => regularAttackValue;
+        
+        [SerializeField]
+        private bool isPositional;
+        public bool IsPositional => isPositional;
+    }
+    
+    [CreateAssetMenu(fileName = "AoETargetAttack", menuName = "Data/Attack/Area of Effect", order = 101)]
+    public class AoEAttack : Attack
+    {
+        [SerializeField]
+        private float attackValueAtCenter;
+        public float AttackValueAtCenter => attackValueAtCenter;
+        
+        [SerializeField]
+        private float radius;
+        public float Radius => radius;
+        
+        [SerializeField]
+        private AnimationCurve falloffByDistance;
+        public AnimationCurve FalloffByDistance => falloffByDistance;
+    }
+
+}
